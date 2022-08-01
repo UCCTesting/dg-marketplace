32bc8f0418190792a40361af02cbb1be6ca099e8
yasinta <yasinta@undercurrent.tech>
2022-08-01 16:28:13 +0700

    create env

--- connection/Connect_Metamask.py
+++ connection/Connect_Metamask.py
@@ -1,6 +1,6 @@
 import os , sys
 import time
-import xlrd
+from dotenv import load_dotenv
 from lib2to3.pgen2 import driver
 from xml.dom.minidom import Element
 from selenium import webdriver
@@ -8,9 +8,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.keys import Keys
 
+load_dotenv()
+
 EXTENSION_PATH = os.getcwd() + '/file/metamask.crx'
 DRIVER_PATH = os.getcwd() + '/file/chromedriver'
-PHRASE_PATH = os.getcwd() + '/file/phrase.xlsx'
 
 def installExtension():
     print('path', EXTENSION_PATH)
@@ -42,29 +43,27 @@ def setupMetamask():
     #load metamask html page
     driver.get('chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#initialize/create-password/import-with-seed-phrase')
 
-    #get pharse.xlsx
-    wb = xlrd.open_workbook(PHRASE_PATH)
-    sheet = wb.sheet_by_index(0)
+
 
     #input phrase on metamask
     driver.switch_to.window(driver.window_handles[-1])
     driver.implicitly_wait(5)
-    driver.find_element(By.ID,'import-srp__srp-word-0').send_keys(sheet.cell_value(0,0))
-    driver.find_element(By.ID,'import-srp__srp-word-1').send_keys(sheet.cell_value(1,0))
-    driver.find_element(By.ID,'import-srp__srp-word-2').send_keys(sheet.cell_value(2,0))
-    driver.find_element(By.ID,'import-srp__srp-word-3').send_keys(sheet.cell_value(3,0))
-    driver.find_element(By.ID,'import-srp__srp-word-4').send_keys(sheet.cell_value(4,0))
-    driver.find_element(By.ID,'import-srp__srp-word-5').send_keys(sheet.cell_value(5,0))
-    driver.find_element(By.ID,'import-srp__srp-word-6').send_keys(sheet.cell_value(6,0))
-    driver.find_element(By.ID,'import-srp__srp-word-7').send_keys(sheet.cell_value(7,0))
-    driver.find_element(By.ID,'import-srp__srp-word-8').send_keys(sheet.cell_value(8,0))
-    driver.find_element(By.ID,'import-srp__srp-word-9').send_keys(sheet.cell_value(9,0))
-    driver.find_element(By.ID,'import-srp__srp-word-10').send_keys(sheet.cell_value(10,0))
-    driver.find_element(By.ID,'import-srp__srp-word-11').send_keys(sheet.cell_value(11,0))
+    driver.find_element(By.ID,'import-srp__srp-word-0').send_keys(os.getenv('PHRASE1'))
+    driver.find_element(By.ID,'import-srp__srp-word-1').send_keys(os.getenv('PHRASE2'))
+    driver.find_element(By.ID,'import-srp__srp-word-2').send_keys(os.getenv('PHRASE3'))
+    driver.find_element(By.ID,'import-srp__srp-word-3').send_keys(os.getenv('PHRASE4'))
+    driver.find_element(By.ID,'import-srp__srp-word-4').send_keys(os.getenv('PHRASE5'))
+    driver.find_element(By.ID,'import-srp__srp-word-5').send_keys(os.getenv('PHRASE6'))
+    driver.find_element(By.ID,'import-srp__srp-word-6').send_keys(os.getenv('PHRASE7'))
+    driver.find_element(By.ID,'import-srp__srp-word-7').send_keys(os.getenv('PHRASE8'))
+    driver.find_element(By.ID,'import-srp__srp-word-8').send_keys(os.getenv('PHRASE9'))
+    driver.find_element(By.ID,'import-srp__srp-word-9').send_keys(os.getenv('PHRASE10'))
+    driver.find_element(By.ID,'import-srp__srp-word-10').send_keys(os.getenv('PHRASE11'))
+    driver.find_element(By.ID,'import-srp__srp-word-11').send_keys(os.getenv('PHRASE12'))
 
     #input password
-    driver.find_element(By.ID,'password').send_keys('password123')
-    driver.find_element(By.ID,'confirm-password').send_keys('password123')
+    driver.find_element(By.ID,'password').send_keys(os.getenv('PASS'))
+    driver.find_element(By.ID,'confirm-password').send_keys(os.getenv('CON_PASS'))
     driver.find_element(By.ID,'create-new-vault__terms-checkbox').click()
     driver.find_element(By.CSS_SELECTOR,'#app-content > div > div.main-container-wrapper > div > div > div.first-time-flow__import > form > button').click()
     driver.find_element(By.CSS_SELECTOR,'#app-content > div > div.main-container-wrapper > div > div > button').click()
